a6eaecb2b1dc43e6f609275954211f4aba1ea8d1
paulotiagocm <paulotiagocm@hotmail.com>
2016-09-23 15:29:35 -0400

    refactor to use psr4

--- src/routes.php
+++ src/routes.php
@@ -1,8 +1,3 @@
 <?php
-// load classes
-spl_autoload_register(function ($class_name) {
-    include $class_name . '.php';
-});
-
 // Routes
 $app->get('/[{name}]', '\App\Controller\MyController:hello');
